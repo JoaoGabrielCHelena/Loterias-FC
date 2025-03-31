44db94ab2a94b223e0d5cb9a899b2791d777a242
João Gabriel C. Helena <joaogchs5789@gmail.com>
2025-03-31 08:27:09 -0300

    final touchups

---
+++ README.md
@@ -0,0 +1,3 @@
+Brainn's lottery challenge
+
+Based on [this](https://github.com/guto-alves/loterias-api), just without the tests because that felt overkill.
--- src/App.tsx
+++ src/App.tsx
@@ -18,44 +18,42 @@ function Dropdown({
     const toggleExpand = () => setExpanded(!expanded)
 
     return (
-        <>
-            <div className={styles.wrapper}>
-                <button className={styles.button} onClick={toggleExpand}>
-                    {selectedItem}
-                    <svg
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="10"
-                        height="6"
-                        viewBox="0 0 10 6"
-                        style={{ rotate: expanded ? "180deg" : "0deg" }}
-                    >
-                        <path
-                            d="M5.633 5.733a.887.887 0 0 1-1.266 0L.262 1.555A.916.916 0 0 1 .068.562a.908.908 0 0 1 .33-.408C.545.054.718 0 .895 0h8.21c.177 0 .35.053.497.154.147.1.262.242.33.408a.926.926 0 0 1-.194.993L5.633 5.733Z"
-                            fill="currentColor"
-                        />
-                    </svg>
-                </button>
-                <ul
-                    className={`${styles.list} ${expanded ? styles.listShow : ""}`}
+        <div className={styles.dropdownWrapper}>
+            <button className={styles.dropdownButton} onClick={toggleExpand}>
+                {selectedItem}
+                <svg
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="10"
+                    height="6"
+                    viewBox="0 0 10 6"
+                    style={{ rotate: expanded ? "180deg" : "0deg" }}
                 >
-                    {dropdownItems.map((i) => {
-                        return (
-                            <li
-                                key={i.key}
-                                onClick={() => {
-                                    i.action()
-                                    toggleExpand()
-                                }}
-                                role="option"
-                            >
-                                <button>{i.display}</button>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
-        </>
+                    <path
+                        d="M5.633 5.733a.887.887 0 0 1-1.266 0L.262 1.555A.916.916 0 0 1 .068.562a.908.908 0 0 1 .33-.408C.545.054.718 0 .895 0h8.21c.177 0 .35.053.497.154.147.1.262.242.33.408a.926.926 0 0 1-.194.993L5.633 5.733Z"
+                        fill="currentColor"
+                    />
+                </svg>
+            </button>
+            <ul
+                className={`${styles.dropdownList} ${expanded ? styles.dropdownListShow : ""}`}
+            >
+                {dropdownItems.map((i) => {
+                    return (
+                        <li
+                            key={i.key}
+                            onClick={() => {
+                                i.action()
+                                toggleExpand()
+                            }}
+                            role="option"
+                        >
+                            <button>{i.display}</button>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
     )
 }
 
@@ -91,17 +89,23 @@ function App() {
 
     const apiOptions = Array.from(optionsName.keys())
 
-    const [selected, setSelected] = useState(apiOptions[0])
-
-    const [dataObj, setDataObj] = useState({
-        dezenas: ["10", "31", "40", "52", "54", "56"],
-        concurso: 2845,
-        data: "27/03/2025",
+    const [dataObj, setDataObj] = useState<{
+        dezenas: string[]
+        concurso: number | string
+        data: string
+    }>({
+        dezenas: [],
+        concurso: 0,
+        data: "unset",
     })
 
-    // getting the path but without the starting / 
+    // getting the path but without the starting /
     // its how it knows which to fetch
     let locale = useLocation().pathname.substring(1)
+
+    const [selected, setSelected] = useState(
+        apiOptions[apiOptions.indexOf(locale)],
+    )
 
     useEffect(() => {
         if (apiOptions.includes(locale)) {
@@ -111,26 +115,33 @@ function App() {
         }
     }, [locale])
 
-    // useEffect(() => {
-    //   const data = async () => {
-    //     try {
-    //       const response = await fetch(`https://loteriascaixa-api.herokuapp.com/api/${selected}/latest`);
-    //       if (!response.ok) {
-    //         throw new Error('Failed to fetch data');
-    //       }
-    //       const result = await response.json();
-    //       setDataObj({
-    //         dezenas: result.dezenas,
-    //         concurso: result.concurso,
-    //         data: result.data,
-    //       })
-    //     } catch (err) {
-    //       setDataObj(err.message); // Handle error
-    //     }
-    //   }
-    //
-    //   data()
-    // }, [selected])
+    useEffect(() => {
+        const data = async () => {
+            try {
+                const response = await fetch(
+                    `https://loteriascaixa-api.herokuapp.com/api/${selected}/latest`,
+                )
+                if (!response.ok) {
+                    throw new Error("Failed to fetch data")
+                }
+                const result = await response.json()
+                setDataObj({
+                    dezenas: result.dezenas,
+                    concurso: result.concurso,
+                    data: result.data,
+                })
+            } catch (err) {
+                setDataObj({
+                    dezenas: ["error"],
+                    concurso: "error",
+                    data: "error",
+                }) // Handle error
+                throw new Error(err.message)
+            }
+        }
+
+        data()
+    }, [selected])
 
     const navigate = useNavigate()
 
